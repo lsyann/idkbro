66fc1515ddf3f1b1761bc759e79e521001b5d7d1
ylau-sim <ylau-sim@f4r4s16.paris.42.school>
2026-07-15 15:01:31 +0200

    sadf

--- main.c
+++ main.c
@@ -19,6 +19,7 @@ typedef struct
 	pthread_mutex_t		*lock;
 	char	*scheduler;
 	struct timeval		tv;
+	int			*burned;
 } coder;
 
 
@@ -27,32 +28,35 @@ coder	*make_coders()
 	coder	*coders;
 	struct timeval	*dongles;
 	pthread_mutex_t	*lock;
-	int	nb;
-	int	i;
+	int	nb[2];
+	int	*burned;
 
-	nb = 3;
+	nb[0] = 2;
 	lock = malloc(sizeof(pthread_mutex_t));
 	pthread_mutex_init(lock, NULL);
-	coders = malloc(nb * sizeof(coder));
-	dongles = malloc(nb * sizeof(struct timeval));
-	i = 0;
-	while (i < nb)
+	coders = malloc(nb[0] * sizeof(coder));
+	dongles = malloc(nb[0] * sizeof(struct timeval));
+	burned = malloc(sizeof(int));
+	burned[0] = 0;
+	nb[1] = 0;
+	while (nb[1] < nb[0])
 	{
-		coders[i].id = i + 1;
-		coders[i].nb_coders = nb;
-		coders[i].burnout_time = 1000;
-		coders[i].compile_time = 200;
-		coders[i].debug_time = 200;
-		coders[i].refactor_time = 200;
-		coders[i].nb_compiles = 3;
-		coders[i].dongle_cooldown = 100;
-		coders[i].dongles = dongles;
-		coders[i].scheduler = "fifo";
-		coders[i].lock = lock;
-		gettimeofday(&coders[i].tv, NULL);
-		gettimeofday(&dongles[i], NULL);
-		dongles[i].tv_sec -= 1;
-		i++;
+		coders[nb[1]].id = nb[1] + 1;
+		coders[nb[1]].nb_coders = nb[0];
+		coders[nb[1]].burnout_time = 1000;
+		coders[nb[1]].compile_time = 200;
+		coders[nb[1]].debug_time = 200;
+		coders[nb[1]].refactor_time = 200;
+		coders[nb[1]].nb_compiles = 3;
+		coders[nb[1]].dongle_cooldown = 100;
+		coders[nb[1]].dongles = dongles;
+		coders[nb[1]].scheduler = "fifo";
+		coders[nb[1]].lock = lock;
+		coders[nb[1]].burned = burned;
+		gettimeofday(&coders[nb[1]].tv, NULL);
+		gettimeofday(&dongles[nb[1]], NULL);
+		dongles[nb[1]].tv_sec -= 1;
+		nb[1]++;
 	}
 	return coders;
 }
@@ -80,6 +84,52 @@ int	time_since(struct timeval tv)
 }
 
 
+int	check_burnout(coder *new, struct timeval tv)
+{
+	if (time_since(tv) > new -> burnout_time)
+	{
+		new -> burned[0] = 1;
+		printf("%d %d burned out\n", time_since(new -> tv), new -> id);
+		return (1);
+	}
+	if (new -> burned[0])
+		return (1);
+	return (0);
+}
+
+
+int	skip_time(int nb, coder *new, struct timeval tv)
+{
+	struct timeval	timer;
+
+	gettimeofday(&timer, NULL);
+	while (nb > time_since(timer))
+	{
+		if (check_burnout(new, tv))
+			return (1);
+		usleep(1000);
+	}
+	return (0);
+}
+
+
+int	dongles_ready(coder *new)
+{
+	int	index1;
+	int	index2;
+	int	cooldown;
+
+	cooldown = new -> dongle_cooldown;
+	index1 = new -> id - 1;
+	index2 = new -> id % new -> nb_coders;
+	if (time_since(new -> dongles[index1]) < cooldown)
+		return (0);
+	if (time_since(new -> dongles[index2]) < cooldown)
+		return (0);
+	return (1);
+}
+
+
 void	*coder_routine(void *idk)
 {
 	coder	*new;
@@ -93,14 +143,13 @@ void	*coder_routine(void *idk)
 	new = (coder *)idk;
 	compiles = 0;
 	gettimeofday(&tv, NULL);
-	printf("thread nb: %d\n", new -> id);
 	while (compiles < new -> nb_compiles)
 	{
 		previous = compiles;
 		while (previous == compiles)
 		{
 			pthread_mutex_lock(new -> lock);
-			if (time_since(new -> dongles[new -> id - 1]) > new -> dongle_cooldown && time_since(new -> dongles[new -> id % new -> nb_coders]) > new -> dongle_cooldown)
+			if (dongles_ready(new))
 			{
 				printf("%d %d has taken a dongle\n", time_since(new -> tv), new -> id);
 				printf("%d %d has taken a dongle\n", time_since(new -> tv), new -> id);
@@ -110,11 +159,8 @@ void	*coder_routine(void *idk)
 			pthread_mutex_unlock(new -> lock);
 			if (previous == compiles)
 				usleep(1000);
-			if (time_since(tv) > new -> burnout_time)
-			{
-				printf("%d %d died\n", time_since(new -> tv), new -> id);
+			if (check_burnout(new, tv))
 				return (NULL);
-			}
 		}
 		gettimeofday(&timer, NULL);
 		while (time_since(timer) < new -> compile_time)
@@ -122,31 +168,14 @@ void	*coder_routine(void *idk)
 			gettimeofday(&(new -> dongles[new -> id - 1]), NULL);
 			gettimeofday(&(new -> dongles[new -> id % (new -> nb_coders)]), NULL);
 			usleep(1000);
+			if (new -> burned[0])
+				return (NULL);
 		}
 		gettimeofday(&tv, NULL);
-		gettimeofday(&timer, NULL);
 		printf("%d %d is debugging\n", time_since(new -> tv), new -> id);
-		while (time_since(timer) < new -> debug_time)
-		{
-			if (time_since(tv) > new -> burnout_time)
-			{
-				printf("%d %d died\n", time_since(new -> tv), new -> id);
-				return (NULL);
-			}
-			usleep(1000);
-		}
-		gettimeofday(&timer, NULL);
+		skip_time(new -> debug_time, new, tv);
 		printf("%d %d is refactoring\n", time_since(new -> tv), new -> id);
-		while (time_since(timer) < new -> refactor_time)
-		{
-			
-			if (time_since(tv) > new -> burnout_time)
-			{
-				printf("%d %d died\n", time_since(new -> tv), new -> id);
-				return (NULL);
-			}
-			usleep(1000);
-		}
+		skip_time(new -> debug_time, new, tv);
 	}
 	printf("%d %d is finished\n", time_since(new -> tv), new -> id);
 	return (NULL);
